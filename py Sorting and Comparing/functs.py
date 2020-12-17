bccb46640befd9ed3e3f3b55dbc09cdf3887aed3
Ethan HunteR <ut2mi2013@gmail.com>
2020-12-17 14:44:05 -0500

    added several new commands, and got the python thing working

--- py Sorting and Comparing/functs.py
+++ py Sorting and Comparing/functs.py
@@ -6,31 +6,52 @@ import os
 class Workspace:
     def __init__(self):
         self.tables = {}
+        self.pythonSource = []  # this is used for embedding python source into the main program
+        self.pythonMode = False # python mode means it'll be executing the following lines as python code
         pass
     def execute(self, line, lineNum):
         self.lineNum = lineNum
-        sourceFrame, command, args = self.breakdownCommand(line)
-
-        if "new_table" == command:
-            self.newFrame(sourceFrame, args)
-        elif "delete" == command:
-            self.delete(sourceFrame, args)
-        elif "open" == command:
-            self.openFile(sourceFrame, args)
-        elif "save_as" == command:
-            self.save(sourceFrame, args)
-        elif "copy_over" == command:
-            self.copyOver(sourceFrame, args)
-        elif "append_to" == command:
-            self.fileAppend(sourceFrame, args)
-        elif "remove_duplicates" == command:
-            self.removeDuplicates(sourceFrame, args)
-        elif "keep_only_duplicates" == command:
-            self.keepDuplicates(sourceFrame, args)
-        elif "remove_lines_matching" == command:
-            self.compareFiles(sourceFrame, args)
+        if self.pythonMode:
+            if "python_end" in line:
+                self.pythonMode = False
+                source = ""
+                for line in self.pythonSource:
+                    source += line
+                exec(source)
+                self.pythonSource = []
+            else:
+                self.pythonSource.append(line)
         else:
-            self.error("command "+command+" does not exist")
+            sourceFrame, command, args = self.breakdownCommand(line)
+
+            if "new_table" == command:
+                self.newFrame(sourceFrame, args)
+            elif "delete" == command:
+                self.delete(sourceFrame, args)
+            elif "open" == command:
+                self.openFile(sourceFrame, args)
+            elif "save_as" == command:
+                self.save(sourceFrame, args)
+            elif "copy_over" == command:
+                self.copyOver(sourceFrame, args)
+            elif "append_to" == command:
+                self.fileAppend(sourceFrame, args)
+            elif "remove_duplicates" == command:
+                self.removeDuplicates(sourceFrame, args)
+            elif "keep_only_duplicates" == command:
+                self.keepDuplicates(sourceFrame, args)
+            elif "remove_lines_matching" == command:
+                self.compareFiles(sourceFrame, args)
+            elif "python_begin" == sourceFrame:
+                self.pythonMode = True
+            elif "copy_column" == command:
+                self.copyColumn(sourceFrame, args)
+            elif "remove_blank_in" == command:
+                self.removeBlankIn(sourceFrame, args)
+            elif "note" == command:
+                pass
+            else:
+                self.error("command "+command+" does not exist")
 
     
 
@@ -70,12 +91,10 @@ class Workspace:
             self.error("table " + sourceFrame + " does not exist!")
         if args[0] not in self.tables:
             self.error("table " + args[0] + " does not exist!")
-        for index, row in self.tables[args[0]].iterrows():  # this is really hacky but clever
-            self.tables[sourceFrame] = self.tables[sourceFrame].append(row)  # appends a line to self.wb1
-            size = len(self.tables[sourceFrame])  # gets the size with an extra added
-            self.tables[sourceFrame] = self.tables[sourceFrame].drop_duplicates(subset=['Email'], keep=False)  # removes duplicates
-            if size == len(self.tables[sourceFrame]):  # and then checks if the size changed. If yes, it was duplicate If not, remove the appended row
-                self.tables[sourceFrame].drop(self.tables[sourceFrame].tail(1).index, inplace=True) # if the size didn't change, remove the appended row
+        table1 = self.tables[sourceFrame]
+        table2 = self.tables[args[0]]
+        table1 = table1[~table1.Email.isin(table2.Email)]
+        self.tables[sourceFrame] = table1
 
 
     def fileAppend(self, sourceFrame, args):
@@ -95,17 +114,21 @@ class Workspace:
         self.tables[sourceFrame] = self.tables[sourceFrame].drop_duplicates(subset=['Email'], keep="first")
 
     def keepDuplicates(self, sourceFrame, args):
+        self.tables[sourceFrame] = self.tables[sourceFrame].sort_values("First Name")
         if sourceFrame not in self.tables:
             self.error("table " + sourceFrame + " does not exist!")
-        duplicatesBool = self.tables[sourceFrame].duplicated(subset=['Email'])
-        duplicates = self.tables[sourceFrame][duplicatesBool]
+        table = self.tables[sourceFrame]
+        duplicatesBool = table.duplicated(subset=['Email'])
+        duplicates = table[duplicatesBool]
         seenDuplicate = []
+        indexOfEmailKey = table.columns.get_loc("Email")
+
         singleInstanceDuplicates = pd.DataFrame()
         for index, row in duplicates.iterrows():
-            if row[2] not in seenDuplicate:
-                seenDuplicate.append(row[2])
+            if row[indexOfEmailKey] not in seenDuplicate:
+                seenDuplicate.append(row[indexOfEmailKey])
                 singleInstanceDuplicates = singleInstanceDuplicates.append(row)
-        neworder = self.tables[sourceFrame].columns
+        neworder = table.columns
         singleInstanceDuplicates = singleInstanceDuplicates.reindex(columns=neworder)
         self.tables[sourceFrame] = singleInstanceDuplicates
 
@@ -113,7 +136,10 @@ class Workspace:
         commandParts = command.split(" ")
         #print(commandParts)
         commandParts = [i.strip("\n ") for i in commandParts]
-        return commandParts[0], commandParts[1], commandParts[2:]
+        if len(commandParts) > 1:
+            return commandParts[0], commandParts[1], commandParts[2:]
+        else:
+            return commandParts[0], None, None
 
     def error(self, text):
         raise SyntaxError("Error on line "+str(self.lineNum)+": "+text)
@@ -129,6 +155,38 @@ class Workspace:
             self.error("path " + pathTo + " does not exist")
         if not os.path.isfile(fullPath):
             self.error("path " + pathTo + " is not a file")
+    def parseColumn(self, lines, start = 0, end = None):
+        colName = ""
+        if end:
+            for i in range(start, end):
+                colName += lines[i] + " "
+            colName = colName.strip()
+        else:
+            for line in lines:
+                colName += line + " "
+            colName = colName.strip()
+        return colName
+    def copyColumn(self, dataframe, args):
+        if len(args) < 3:
+            self.error("copy_column requires a source and destination columnn, seperated by \"to\"!")
+        else:
+            frame = self.tables[dataframe]
+            toLocation = args.index("to")
 
+
+            col1Name = self.parseColumn(args, end=toLocation)
+            col2Name = self.parseColumn(args, start = toLocation+1, end=len(args))
+            frame[col2Name] = frame[col1Name]
+
+    def removeBlankIn(self, dataframe, args):
+        colName = self.parseColumn(args)
+        table = self.tables[dataframe]
+        table = table.dropna(how = "any", subset = ["Contact2"])
+        self.tables[dataframe] = table
+        print("Ho")
+
+
+
+ta = open("testemails_Optinsurvey2.xlsx")
 
 
